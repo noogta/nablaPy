a4a843fb314323e92c9dfda139152be83f6b33bd
Teggar Farès <teggarfare@cy-tech.fr>
2023-06-06 09:45:51 +0200

    Update

--- RadarData.py
+++ RadarData.py
@@ -1,28 +1,54 @@
 import numpy as np
 import matplotlib.pyplot as plt
-
+import os
 #Constante Globale
 c = 299792458 # Vitesse de la lumière dans le vide en m/s
+
+
 class RadarData:
+    """RadarData: Classe contenant toutes les méthodes permettant avoir accès aux différentes données"""
     def __init__(self,path: str):
+        """
+    Constructeur de la classe RadarData.
+
+    Args:
+                path (str): chemin du fichier
+        """
         self.path = path
     
     def rd_mat_high_freq(self):
+        """
+    Méthode permettant de récupérer la zone sondée à partir d'un fichier .rd3 ou .rd7.
+
+    Return:
+        Retourne le tableau numpy contenant les données de la zone sondée.
+    """
         try:
             if(self.path.endswith(".rd3")):
                 with open(self.path, mode='rb') as rd3data:  # Ouvrir le fichier en mode binaire
                     byte_data = rd3data.read()
                 rd3 = np.frombuffer(byte_data, dtype=np.int16) #rd3 est codé sur 2 octets
                 rd3 = rd3.reshape(self.get_rad()[0], self.get_rad()[1])
+                rd3 = rd3.transpose()
                 print("Taille du tableau :", rd3.shape)
                 return rd3
             elif(self.path.endswith(".rd7")):
                 with open(self.path, mode='rb') as rd7data:  # Ouvrir le fichier en mode binaire
                     byte_data = rd7data.read()
-                rd7 = np.frombuffer(byte_data, dtype=np.int32)
-                rd7 = rd7.reshape(self.get_rad()[0], self.get_rad()[1]) # rd7 est codé sur 3 octets
+                rd7 = np.frombuffer(byte_data, dtype=np.int32) #rd7 est codé sur 3 octets
+                rd7 = rd7.reshape(self.get_rad()[0], self.get_rad()[1])
+                rd7 = rd7.transpose()
                 print("Taille du tableau :", rd7.shape)
                 return rd7
+            
+            #README
+            # Si vous souhaitez rajouter d'autres format:
+            # -1 Ajouter elif(self.path.endswith(".format")):
+            # -2 Veuillez vous renseigner sur la nature de vos données binaire, héxadécimal ...
+            # -3 Lire les fichiers et ensuite les transférer dans un tableau numpy
+            # -4 Redimmensionnez votre tableau à l'aide du nombre de samples et de traces
+            # -5 Transposez le tableau
+
         except Exception as e:
             print("Erreur lors de la lecture du fichier:", str(e))
 
@@ -44,13 +70,14 @@ class RadarData:
                 value = line.split(':')[1]
                 value_trace = int(value)
                 break # on arrête la boucle car nous avons trouvé les deux variables souhaités
-        return value_sample, value_trace
+        return value_trace, value_sample
         
 
 
 
 path_rd3_high = "/home/cytech/Stage/Mesures/JOUANY1/JOUANY1_0001_1.rd7"
 radardata = RadarData(path_rd3_high)
-radardata.rd_mat_high_freq()
-        
-
+rd = radardata.rd_mat_high_freq()
+plt.imshow(rd,)
+plt.show()
+#os.system("clear")
